0b3088f0fc479d6c5ce9197b12cc7e0b3575b19e
Dylan Quiroga Arancibia <dylan.quiroga@userena.cl>
2023-05-24 21:00:46 -0400

    Toast

--- app/src/main/java/com/example/apppraxisii/Ejercicio1.java
+++ app/src/main/java/com/example/apppraxisii/Ejercicio1.java
@@ -3,12 +3,38 @@ package com.example.apppraxisii;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class Ejercicio1 extends AppCompatActivity {
+    private VG varGlob;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ejercicio1);
+
+        varGlob = (VG) getApplicationContext();
+        ImageView Bueno = findViewById(R.id.imagePulgarArriba);
+        ImageView Malo = findViewById(R.id.imagePulgarAbajo);
+
+        Bueno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(getApplicationContext(),"Incorrecto", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
+        Malo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(getApplicationContext(),"Correcto", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/apppraxisii/MainActivity.java
+++ app/src/main/java/com/example/apppraxisii/MainActivity.java
@@ -2,25 +2,27 @@ package com.example.apppraxisii;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private VG varGlob;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        VG variableGlobal = (VG) getApplicationContext();
+        varGlob = (VG) getApplicationContext();
         ImageView imageComenzar = findViewById(R.id.imageComenzar);
         ImageView imageSalir = findViewById(R.id.imageSalir);
 
         imageComenzar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                System.out.println("Imagen 1");
+                ejercicio1(view);
             }
         });
 
@@ -32,4 +34,10 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+    public void ejercicio1(View view) {
+        Intent intent = new Intent(this, Ejercicio1.class);
+        startActivity(intent);
+    }
+
 }
